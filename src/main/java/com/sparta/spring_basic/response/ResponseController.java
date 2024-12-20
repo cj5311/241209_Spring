104686ab101f14d9bc4bc05905809ef30e62d6b4
Cheongjo <103722439+cj5311@users.noreply.github.com>
2024-12-20 22:57:48 +0900

    How to return data to the client

--- src/main/java/com/sparta/spring_basic/response/ResponseController.java
+++ src/main/java/com/sparta/spring_basic/response/ResponseController.java
@@ -4,27 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+// @Controller: View 반환할때 vs RestController : data 반환할때
+@RestController
 @RequestMapping("/response")
 public class ResponseController {
-    // [Response header]
-    //   Content-Type: text/html
-    // [Response body]
-    //   {"name":"Robbie","age":95}
+
+
     @GetMapping("/json/string")
-    @ResponseBody
+//    @ResponseBody // @Controller 에서 @RestController 로 변경시 생략가능
     public String helloStringJson() {
         return "{\"name\":\"Robbie\",\"age\":95}";
+        // 서버에서 json 형식을 읽을 수 없기 때문에 String으로 처리
+        // [Response header] : Content-Type: text/html
+        // [Response body] :  {"name":"Robbie","age":95}
     }
 
-    // [Response header]
-    //   Content-Type: application/json
-    // [Response body]
-    //   {"name":"Robbie","age":95}
     @GetMapping("/json/class")
-    @ResponseBody
+//    @ResponseBody // html 이 아닌 데이터 반환임을 설정
     public Star helloClassJson() {
         return new Star("Robbie", 95);
+        // [Response header] : Content-Type: application/json
+        // [Response body] :  {"name":"Robbie","age":95}
     }
 }
