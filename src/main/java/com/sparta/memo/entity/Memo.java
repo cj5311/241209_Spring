1a211c021e5af57922486bcfad2f01f541b110fb
Cheongjo <103722439+cj5311@users.noreply.github.com>
2024-12-23 00:56:15 +0900

    JPA in SpringBoot

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,22 +1,23 @@
 package com.sparta.memo.entity;
 
 import com.sparta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashMap;
-import java.util.Map;
-
-//데이터와 소통할 클래스
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
 public class Memo {
-
-    // 저장할 데이터 정의 : id, username, contents
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "username", nullable = false)
     private String username;
+    @Column(name = "contents", nullable = false, length = 500)
     private String contents;
 
     public Memo(MemoRequestDto requestDto) {
@@ -25,7 +26,7 @@ public class Memo {
     }
 
     public void update(MemoRequestDto requestDto) {
-        this.contents = requestDto.getContents();
         this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
     }
 }
